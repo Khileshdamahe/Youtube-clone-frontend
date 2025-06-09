b2cedd2b6b62db8a3bfee53e2f7d63327f6649a2
khileshdamahe <khileshdamahekd2000@gmail.com>
2025-06-09 21:11:38 +0530

    added homeoption in homepage

--- src/components/HomePage/homePage.js
+++ src/components/HomePage/homePage.js
@@ -3,8 +3,22 @@ import "./homePage.css"
 
 
 const HomePage = () => {
+
+  const options = ["All", "Twenty20 Cricket", "Music", "Live", "Mixes", "Gaming", "Debates", "Coke Studio Pakistan", "Democracy", "Pakistani dramas", "Comedy", "Pakistani dramas", "Comedy", "Pakistani dramas", "Comedy",];
   return (
-    <div>HomePage</div>
+    <div className='homePage'>
+      <div className="homePage_options">
+         {
+          options.map((item, index) => {
+            return (
+              <div key={index} className="homePage_option">
+                {item}
+              </div>
+            );
+          })
+        }
+      </div>
+    </div>
   )
 }
 
