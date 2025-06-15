5a1913c7f07e87023c4718f00f00d247772082e7
khileshdamahe <khileshdamahekd2000@gmail.com>
2025-06-15 21:14:48 +0530

    added state in video.js

--- src/pages/Video/video.js
+++ src/pages/Video/video.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './video.css';
 import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
 import ThumbDownAltOutlinedIcon from '@mui/icons-material/ThumbDownAltOutlined';
 import { Link } from 'react-router-dom';
 
 const Video = () => {
+    const [message, setMessage] = useState("");
+    console.log(message);
     return (
         <div className='video'>
             <div className="videoPostSection">
@@ -54,7 +56,7 @@ const Video = () => {
                         <div className="youtubeSelfComment">
                             <img className='video_youtubeSelfCommentProfile' src='https://media.istockphoto.com/id/2027127656/photo/vibrant-colored-closed-wooden-doors-in-a-row-on-blue-sky-and-sea-background-choice-and.webp?a=1&b=1&s=612x612&w=0&k=20&c=i_Ymb_iVtRFAdccY2x7dRarxDCyII1k2XEDU3MVGL4w=' alt='khilesh' />
                             <div className='addAComment'>
-                                <input type='text' className='addAcommentInput' placeholder='Add a comment' />
+                                <input type='text' value={message} onChange={(e) => { setMessage(e.target.value) }} className='addAcommentInput' placeholder='Add a comment' />
                                 <div className='cancelSubmitComment'>
                                     <div className='cancelComment'>Cancel</div>
                                     <div className='cancelComment'>Comment</div>
@@ -131,7 +133,7 @@ const Video = () => {
             <div className="videoSuggestions">
                 <div className="videoSuggestionsBlock">
                     <div className="video_suggetion_thumbnail">
-                        <img src="https://th.bing.com/th/id/OIP.8gLtXrl4KYPfPA6QyMnlUwHaEK?w=304&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" className='video_suggetion_thumbnail_img' alt='khilesh'/>
+                        <img src="https://th.bing.com/th/id/OIP.8gLtXrl4KYPfPA6QyMnlUwHaEK?w=304&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" className='video_suggetion_thumbnail_img' alt='khilesh' />
                     </div>
                     <div className="video_suggetions_About">
                         <div className="video_suggetions_About_title">T20 2024 Worldcup Final IND vs SA Last 5 overs #cricket #india</div>
@@ -142,7 +144,7 @@ const Video = () => {
 
                 <div className="videoSuggestionsBlock">
                     <div className="video_suggetion_thumbnail">
-                        <img src="https://th.bing.com/th/id/OIP.8gLtXrl4KYPfPA6QyMnlUwHaEK?w=304&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" className='video_suggetion_thumbnail_img' alt='khilesh'/>
+                        <img src="https://th.bing.com/th/id/OIP.8gLtXrl4KYPfPA6QyMnlUwHaEK?w=304&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" className='video_suggetion_thumbnail_img' alt='khilesh' />
                     </div>
                     <div className="video_suggetions_About">
                         <div className="video_suggetions_About_title">T20 2024 Worldcup Final IND vs SA Last 5 overs #cricket #india</div>
@@ -153,7 +155,7 @@ const Video = () => {
 
                 <div className="videoSuggestionsBlock">
                     <div className="video_suggetion_thumbnail">
-                        <img src="https://th.bing.com/th/id/OIP.8gLtXrl4KYPfPA6QyMnlUwHaEK?w=304&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" className='video_suggetion_thumbnail_img' alt='khilesh'/>
+                        <img src="https://th.bing.com/th/id/OIP.8gLtXrl4KYPfPA6QyMnlUwHaEK?w=304&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" className='video_suggetion_thumbnail_img' alt='khilesh' />
                     </div>
                     <div className="video_suggetions_About">
                         <div className="video_suggetions_About_title">T20 2024 Worldcup Final IND vs SA Last 5 overs #cricket #india</div>
@@ -164,7 +166,7 @@ const Video = () => {
 
                 <div className="videoSuggestionsBlock">
                     <div className="video_suggetion_thumbnail">
-                        <img src="https://th.bing.com/th/id/OIP.8gLtXrl4KYPfPA6QyMnlUwHaEK?w=304&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" className='video_suggetion_thumbnail_img' alt='khilesh'/>
+                        <img src="https://th.bing.com/th/id/OIP.8gLtXrl4KYPfPA6QyMnlUwHaEK?w=304&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" className='video_suggetion_thumbnail_img' alt='khilesh' />
                     </div>
                     <div className="video_suggetions_About">
                         <div className="video_suggetions_About_title">T20 2024 Worldcup Final IND vs SA Last 5 overs #cricket #india</div>
