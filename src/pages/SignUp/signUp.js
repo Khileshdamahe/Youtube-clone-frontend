4d839dd89bb7a4a5c51dc1c51b4c34b6b31c4517
khileshdamahe <khileshdamahekd2000@gmail.com>
2025-06-15 20:39:13 +0530

    added state in signup page

--- src/pages/SignUp/signUp.js
+++ src/pages/SignUp/signUp.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './signUp.css';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+    const [signUpFiled, setSignUpField] = useState({ "channelName": "", "userName": "", "password": "", "about": "", "profilePic": "" });
+    const [uploadedImageUrl, setUploadedImageUrl] = useState("https://th.bing.com/th/id/OIP.Wy2uo_y-ttULYs4chLmqSAAAAA?rs=1&pid=ImgDetMain");
+    console.log(signUpFiled)
+
+
+
+    const handleInputField = (event, name) => {
+        setSignUpField({
+            ...signUpFiled, [name]: event.target.value
+        })
+    }
+
+
     return (
         <div className='signUp'>
             <div className="signup_card">
@@ -13,17 +26,17 @@ const SignUp = () => {
                 </div>
 
                 <div className="signUp_Inputs">
-                    <input type='text' className='signUp_Inputs_inp' placeholder='Channel Name' />
-                    <input type='text' className='signUp_Inputs_inp' placeholder='User Name' />
-                    <input type='password' className='signUp_Inputs_inp' placeholder='Password' />
-                    <input type='text' className='signUp_Inputs_inp' placeholder='About Your Channel' />
+                    <input type='text' className='signUp_Inputs_inp' value={signUpFiled.channelName} onChange={(e) => { handleInputField(e, "channelName") }} placeholder='Channel Name' />
+                    <input type='text' className='signUp_Inputs_inp' value={signUpFiled.userName} onChange={(e) => { handleInputField(e, "userName") }} placeholder='User Name' />
+                    <input type='password' className='signUp_Inputs_inp' value={signUpFiled.password} onChange={(e) => { handleInputField(e, "password") }} placeholder='Password' />
+                    <input type='text' className='signUp_Inputs_inp' value={signUpFiled.about} onChange={(e) => { handleInputField(e, "about") }} placeholder='About Your Channel' />
 
 
 
                     <div className="image_upload_signup">
                         <input type='file' />
                         <div className="image_upload_signup_div">
-                            <img className='image_default_signUp' src='https://randomwordgenerator.com/img/picture-generator/50e5d6474e5bb10ff3d8992cc12c30771037dbf85254784d712f7dd59245_640.jpg' alt='khilesh' />
+                            <img className='image_default_signUp' src={uploadedImageUrl} alt='khilesh' />
                         </div>
 
 
