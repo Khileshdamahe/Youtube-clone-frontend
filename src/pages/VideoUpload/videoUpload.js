80ba3626b94fa82c86294fcae9122b848b8671db
khileshdamahe <khileshdamahekd2000@gmail.com>
2025-06-15 13:53:04 +0530

    added videoUpload js properly and linked the home button with route

--- src/pages/VideoUpload/videoUpload.js
+++ src/pages/VideoUpload/videoUpload.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import './videoUpload.css';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const VideoUpload = () => {
     return (
-        <div className='home'>
-          VideoUpload
+        <div className='videoUpload'>
+            <div className="uploadBox">
+                <div className="uploadVideoTitle">
+                    <YouTubeIcon sx={{ fontSize: "54px", color: "red" }} />
+
+                    Upload Video
+                </div>
+
+                <div className="uploadForm">
+                    <input type='text' placeholder='Title of Video' className='uploadFormInputs' />
+                    <input type='text' placeholder='Description' className='uploadFormInputs' />
+                    <input type='text' placeholder='Category' className='uploadFormInputs' />
+                    <div>Thumbnail<input type='file' accept='image/*' /></div>
+                    <div>Video <input type='file' accept='video/mp4,video/webm,video/*' /></div>
+
+                </div>
+
+
+
+
+                <div className="uploadBtns">
+                    <div className="uploadBtn-form">Upload</div>
+                    <Link to={'/'} className="uploadBtn-form">Home</Link>
+
+                </div>
+
+            </div>
         </div>
     )
 }
