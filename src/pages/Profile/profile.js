bc17af824b394ad8f2396fb8396a6a2f0c73075e
khileshdamahe <khileshdamahekd2000@gmail.com>
2025-06-19 13:34:47 +0530

    integrated profile page with backend

--- src/pages/Profile/profile.js
+++ src/pages/Profile/profile.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './profile.css'
 import SideNavbar from '../../components/SideNavbar/sideNavbar'
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const Profile = ({ sideNavbar }) => {
+    const { id } = useParams();
+    const [data, setData] = useState([]);
+    const [user, setUser] = useState(null);
+    const fetchProfileData = async () => {
+        axios.get(`http://localhost:4000/api/${id}/channel`).then((response) => {
+            console.log(response)
+            setData(response.data.video);
+            setUser(response.data.video[0]?.user)
+        }).catch(err => {
+            console.log(err);
+        })
+
+
+    }
+    useEffect(() => {
+        fetchProfileData()
+    }, [])
+
+
+
+
+
     return (
         <div className='profile'>
             <SideNavbar sideNavbar={sideNavbar} />
@@ -13,15 +36,15 @@ const Profile = ({ sideNavbar }) => {
 
                 <div className="profile_top_section">
                     <div className="profile_top_section_profile">
-                        <img className='profile_top_section_img' src="https://randomuser.me/api/portraits/lego/2.jpg" alt="" />
+                        <img className='profile_top_section_img' src={user?.profilePic} alt="" />
                     </div>
                     <div className="profile_top_section_About">
-                        <div className='profile_top_section_About_Name'>CodingKhileshDamahe</div>
+                        <div className='profile_top_section_About_Name'>{user?.channelName}</div>
                         <div className="profile_top_section_info">
-                            @User1 . 4 Videos
+                            {user?.userName} . {data.length} Videos
                         </div>
                         <div className="profile_top_section_info">
-                            About Section of Channel
+                            {user?.about}
                         </div>
 
                     </div>
@@ -31,45 +54,25 @@ const Profile = ({ sideNavbar }) => {
                 <div className="profile_videos">
                     <div className="profile_videos_title">Videos &nbsp; <ArrowRightIcon /></div>
                     <div className="profileVideos">
-                        {/* -------div for video-------- */}
 
-                        <Link to={`/watch/98`} className="profileVideo_block">
-                            <div className="profileVideo_block_thumbnail">
-                                <img className='profileVideo_block_thumbnail_img' src='https://randomuser.me/api/portraits/lego/4.jpg' alt='khilesh' />
-                            </div>
-                            <div className="profileVideo_block_detail">
-                                <div className="profileVideo_block_detail_name">Video Title</div>
-                                <div className="profileVideo_block_detail_about">Create at 2024-09-12</div>
-                            </div>
-
-
-                        </Link>
-                        {/* -------div for video-------- */}
-
-                        <Link to={`/watch/98`} className="profileVideo_block">
-                            <div className="profileVideo_block_thumbnail">
-                                <img className='profileVideo_block_thumbnail_img' src='https://randomuser.me/api/portraits/lego/4.jpg' alt='khilesh' />
-                            </div>
-                            <div className="profileVideo_block_detail">
-                                <div className="profileVideo_block_detail_name">Video Title</div>
-                                <div className="profileVideo_block_detail_about">Create at 2024-09-12</div>
-                            </div>
+                        {
+                            data.map((item, key) => {
+                                return (
+                                    <Link to={`/watch/${item._id}`} className="profileVideo_block">
+                                        <div className="profileVideo_block_thumbnail">
+                                            <img className='profileVideo_block_thumbnail_img' src={item?.thumbnail} alt='khilesh' />
+                                        </div>
+                                        <div className="profileVideo_block_detail">
+                                            <div className="profileVideo_block_detail_name">{item?.title}</div>
+                                            <div className="profileVideo_block_detail_about">Create at {item?.createdAt.slice(0, 10)}</div>
+                                        </div>
 
 
-                        </Link>
-                        {/* -------div for video-------- */}
+                                    </Link>
+                                )
+                            })
+                        }
 
-                        <Link to={`/watch/98`} className="profileVideo_block">
-                            <div className="profileVideo_block_thumbnail">
-                                <img className='profileVideo_block_thumbnail_img' src='https://randomuser.me/api/portraits/lego/4.jpg' alt='khilesh' />
-                            </div>
-                            <div className="profileVideo_block_detail">
-                                <div className="profileVideo_block_detail_name">Video Title</div>
-                                <div className="profileVideo_block_detail_about">Create at 2024-09-12</div>
-                            </div>
-
-
-                        </Link>
                     </div>
                 </div>
             </div>
