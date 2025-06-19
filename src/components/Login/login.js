a7dce90d95f4b4ec0a4723b4c27e4adc868e664b
khileshdamahe <khileshdamahekd2000@gmail.com>
2025-06-19 14:29:10 +0530

    integrated login with backend and addede loader and refreshed the page after login and storing token in local storage

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import './login.css';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 
 
 const Login = ({ setLoginModal }) => {
@@ -15,6 +21,25 @@ const Login = ({ setLoginModal }) => {
             ...loginField, [name]: event.target.value
         })
     }
+
+    const handleLoginFun = async () => {
+        setLoader(true)
+        axios.post("http://localhost:4000/auth/login", loginField).then((resp => {
+            setLoader(false)
+            // console.log(resp)
+            localStorage.setItem("token", resp.data.token)
+            localStorage.setItem("userId", resp.data.user._id)
+            localStorage.setItem("userProfilePic", resp.data.user.profilePic)
+            window.location.reload();
+        })).catch(err => {
+            toast.error("Invalid Credentials")
+            console.log(err)
+            setLoader(false)
+        })
+    }
+
+
+
     return (
         <div className='login'>
             <div className="login_card">
@@ -32,12 +57,19 @@ const Login = ({ setLoginModal }) => {
 
                 </div>
                 <div className="login_buttons">
-                    <div className="login-btn">Login</div>
+                    <div className="login-btn" onClick={handleLoginFun} >Login</div>
                     <Link to={'/signup'} onClick={() => setLoginModal()} className="login-btn">SignUp</Link>
                     <div className="login-btn" onClick={() => setLoginModal()}>Cancel</div>
                 </div>
-            </div>
 
+                {loader && <Box sx={{ width: '100%' }}>
+                    <LinearProgress />
+                </Box> }
+
+
+
+            </div>
+            <ToastContainer />
         </div>
     )
 }
